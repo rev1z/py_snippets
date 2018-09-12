a3b1d7f31c91dc100c71f803942cb7260fb22698
rev1z <a.bektepov@gmail.com>
2018-09-12 22:10:22 +0300

    Create final_class.py

---
+++ final_class.py
@@ -0,0 +1,16 @@
+#Final class realisation (can not be inherited from)
+
+
+class Final(type):
+    def __new__(cls, name, bases, classdict):
+        for b in bases:
+            if isinstance(b, Final):
+                raise TypeError("type '{0}' is not an acceptable base type".format(b.__name__))
+        return type.__new__(cls, name, bases, dict(classdict))
+
+class FinalClass(metaclass=Final):
+    pass
+
+class DummyClass(FinalClass): 
+    pass 
+# will cause TypeError 
